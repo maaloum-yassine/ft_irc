c960a242d21e30e635933f8f9b13e2c4d8622d53
Yassine MAALOUM <maaloum.yassine@gmail.com>
2024-07-15 08:52:31 +0100

    update

--- src/Parse/Parse.cpp
+++ src/Parse/Parse.cpp
@@ -6,7 +6,7 @@
 /*   By: ymaaloum <ymaaloum@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 04:09:12 by ymaaloum          #+#    #+#             */
-/*   Updated: 2024/07/05 05:18:07 by ymaaloum         ###   ########.fr       */
+/*   Updated: 2024/07/15 08:52:04 by ymaaloum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ Parse ::Parse(){};
 
 void	Parse :: ParsePort(const std :: string& port)
 {
-	if (!CheckDigit(port) || atoi(port.c_str()) > 65535 )
+
+	if (port.empty() || !CheckDigit(port) || atoi(port.c_str()) > 65535 )
 		throw std :: string ("ERREUR FOR PORT !");
 }
 Parse :: ~Parse()
